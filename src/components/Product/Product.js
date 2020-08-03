58e515fb1c8b3eae3378410d77f022563b35cfcc
Rajan Gurung <rajangrg1@hotmail.com>
2020-08-03 14:19:39 +1200

    Product component, UI & UX design completed

---
+++ src/components/Product/Product.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Card,  Image, Rating, Button } from "semantic-ui-react";
+import "./Product.css";
+
+function Product({ id, title, price, rating, imageUrl }) {
+  return (
+    <div className="product">
+      <Card className="product__card">
+        <Image
+          className="product__image"
+         
+          centered
+          src={imageUrl}
+        />
+        <Card.Content>
+          <Card.Header className="product__title">{title}</Card.Header>
+          <Card.Meta>
+            <Rating icon="star" defaultRating={rating} maxRating={5} />
+          </Card.Meta>
+          <Card.Description>
+            <span className="product__price">${price}</span>
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra className="product__footer" >
+          <Button inverted className="product__button">
+            ADD TO BASKET
+          </Button>
+        </Card.Content>
+      </Card>
+    </div>
+  );
+}
+
+export default Product;
--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -1,12 +1,72 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import Product from "../../components/Product/Product";
+import { Container, Grid } from "semantic-ui-react";
 
 function Home() {
-    return (
-        <div>
-           <h1>Home page</h1>
-        </div>
-    )
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    //sample data will be replaced with db data in future.
+    setProduct([
+      {
+        id: 1,
+        title: "The Power of Now: A Guide to Spiritual Enlightenment",
+        price: 9.5,
+        rating: 5,
+        imageUrl:
+          "https://images-na.ssl-images-amazon.com/images/I/41WIbflfG2L._SX323_BO1,204,203,200_.jpg",
+      },
+      {
+        id: 2,
+        title:
+          "Squier by Fender Jim Root Telecaster Electric Guitar - Laurel Fingerboard - Flat Black",
+        price: 1500,
+        rating: 3,
+        imageUrl:
+          "https://images-na.ssl-images-amazon.com/images/I/51724XHDqpL._AC_SL1000_.jpg",
+      },
+      {
+        id: 3,
+        title:
+          "FEA Men's Tool Spectre Spiral Vicarious Men's T-Shirt,Black,X-Large",
+        price: 200,
+        rating: 4,
+        imageUrl:
+          "https://images-na.ssl-images-amazon.com/images/I/51Hgu-mKe%2BL._AC_UX466_.jpg",
+      },
+      {
+        id: 4,
+        title: "Led Zeppelin I (Remastered Original Vinyl)",
+        price: 29.27,
+        rating: 5,
+        imageUrl:
+          "https://images-na.ssl-images-amazon.com/images/I/816FdtquvwL._SX425_.jpg",
+      },
+    ]);
+  }, []);
+  console.log(product);
+
+  return (
+    <div>
+      <Container>
+        <Grid container columns={4} doubling stackable>
+          {product.map((product) => {
+            return (
+              <Grid.Column stretched >
+                <Product
+                  key={product.id}
+                  title={product.title}
+                  price={product.price}
+                  rating={product.rating}
+                  imageUrl={product.imageUrl}
+                ></Product>
+              </Grid.Column>
+            );
+          })}
+        </Grid>
+      </Container>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
