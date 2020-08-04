2a3addf4ccd5fb52230c3e6e718e2f8d6936b983
Rajan Gurung <rajangrg1@hotmail.com>
2020-08-04 14:10:23 +1200

    UI design Login container finished

--- src/container/Login/Login.js
+++ src/container/Login/Login.js
@@ -1,11 +1,29 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Card, Form, Button, Grid } from "semantic-ui-react";
+import "./Login.css";
 
 function Login() {
   return (
-    <div>
+    <div className="login">
       <Container>
-        <h1>coming soon..</h1>
+        <Grid centered columns={3}>
+          <Grid.Column>
+          <h2>Sign In</h2>
+            <Card>
+              <Form className="login__form">
+                <Form.Field>
+                  <label>E-mail</label>
+                  <input placeholder="First Name" text="email" required />
+                </Form.Field>
+                <Form.Field>
+                  <label>Password</label>
+                  <input placeholder="Last Name" text="password" required />
+                </Form.Field>
+                <Button color="green" type="submit">Login</Button>
+              </Form>
+            </Card>
+          </Grid.Column>
+        </Grid>
       </Container>
     </div>
   );
