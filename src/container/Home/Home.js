b8b883b3061c31690fecf022d24a33c72ad40a8d
Rajan Gurung <rajangrg1@hotmail.com>
2020-08-05 13:29:42 +1200

    added Loder on state change - Animation

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -1,39 +1,49 @@
 import React, { useState, useEffect } from "react";
 import Product from "../../components/Product/Product";
-import { Container, Grid } from "semantic-ui-react";
-import './Home.css'
-import {db} from '../../Firebase/FirebaseConfig'
+import { Container, Grid,Segment,Dimmer, Loader } from "semantic-ui-react";
+import "./Home.css";
+import { db } from "../../Firebase/FirebaseConfig";
 
 function Home() {
   const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    db.collection('Products').onSnapshot(snapshot =>{
-      setProduct(snapshot.docs.map(doc=>doc.data()))
-    })
+    db.collection("Products").onSnapshot((snapshot) => {
+      setProduct(snapshot.docs.map((doc) => doc.data()));
+      setLoading(false);
+    });
   }, [setProduct]);
   console.log(product);
 
   return (
     <div className="home">
-      <Container>
-        <Grid container columns={4} doubling stackable>
-          {product.map((product) => {
-            return (
-              <Grid.Column stretched key={product.id}>
-                <Product
-                id={product.id}
-                  key={product.id}
-                  title={product.title}
-                  price={product.price}
-                  rating={product.rating}
-                  imageUrl={product.imageUrl}
-                ></Product>
-              </Grid.Column>
-            );
-          })}
-        </Grid>
-      </Container>
+      {loading ? (
+        <Segment className="home__segment">
+          <Dimmer active inverted>
+            <Loader size="large" className="home__loaderMessage">Loading...</Loader>
+          </Dimmer>
+        </Segment>
+      ) : (
+        <Container>
+          <Grid container columns={4} doubling stackable>
+            {product.map((product, index) => {
+              return (
+                <Grid.Column stretched key={index}>
+                  <Product
+                    id={product.id}
+                    key={product.id}
+                    title={product.title}
+                    price={product.price}
+                    rating={product.rating}
+                    imageUrl={product.imageUrl}
+                  ></Product>
+                </Grid.Column>
+              );
+            })}
+          </Grid>
+        </Container>
+      )}
     </div>
   );
 }
