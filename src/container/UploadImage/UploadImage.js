df83ea31e31727ed9640de0a60611817467fcb7d
Rajan Gurung <rajangrg1@hotmail.com>
2020-08-06 18:51:01 +1200

    Responsive design fixed for upload product page

--- src/container/UploadImage/UploadImage.js
+++ src/container/UploadImage/UploadImage.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import AddProduct from '../../components/AddProduct/AddProduct'
+import './UploadImage.css'
 
 function UploadImage() {
     return (
-        <div>
+        <div className="uploadImage">
             <AddProduct></AddProduct>
         </div>
     )
